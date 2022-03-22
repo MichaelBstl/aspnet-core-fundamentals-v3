396d1a12e59834a575443471f2fb160c46c04c8d
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-03-22 08:27:05 -0500

    step11

--- SimpleCrm1/SimpleCrm1.Web/Controllers/AboutController.cs
+++ SimpleCrm1/SimpleCrm1.Web/Controllers/AboutController.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SimpleCrm1.Web.Controllers
 {
+    [Route("about")]
     public class AboutController
     {
+        [Route("")]
         public string Phone(string id)
         {
             return "999-999-9999";
         }
+        [Route("Address")]
         public string Address(string id)
         {
             return "USA";
