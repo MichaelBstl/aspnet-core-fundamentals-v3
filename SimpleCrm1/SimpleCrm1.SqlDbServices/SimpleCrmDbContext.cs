3beda01abe83f9ab5c435a30676cae7111bb5d4c
Michael L Bryant <MichaelB_1968@yahoo.com>
2022-03-24 14:44:59 -0500

    step21

---
+++ SimpleCrm1/SimpleCrm1.SqlDbServices/SimpleCrmDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCrm1.SqlDbServices
+{
+    public class SimpleCrmDbContext : DbContext
+    {
+        public SimpleCrmDbContext(DbContextOptions<SimpleCrmDbContext> options)
+            : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ SimpleCrm1/SimpleCrm1.SqlDbServices/SqlCustomerData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCrm1.SqlDbServices
+{
+    public class SqlCustomerData : ICustomerData
+    {
+        private readonly SimpleCrmDbContext dbContext;
+        public SqlCustomerData(SimpleCrmDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Customer Get(int id)
+        {
+            return dbContext.Customers.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            return dbContext.Customers.ToList();
+        }
+
+        public void Save(Customer customer)
+        {
+            dbContext.Customers.Add(customer);
+            dbContext.SaveChanges();
+            return;
+        }
+    }
+}
